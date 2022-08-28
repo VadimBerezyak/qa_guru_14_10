7e5ccbe14045dc8025b940a88c4c6646016889ff
VadimBerezyak <89918671+VadimBerezyak@users.noreply.github.com>
2022-08-28 19:17:00 +0300

    allure was added

---
+++ src/test/java/qa/guru/allure/SelenideTest.java
@@ -0,0 +1,25 @@
+package qa.guru.allure;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.By.linkText;
+
+public class SelenideTest {
+    @Test
+public void issueSearchTest(){
+open("https://github.com");
+$(".header-search-input").click();
+$(".header-search-input").setValue("eroshenkoam/allure-example");
+$(".header-search-input").pressEnter();
+$(linkText("eroshenkoam/allure-example")).click();
+$("#issues-tab").click();
+$(withText("#80")).should(Condition.exist);
+}
+
+
+}
