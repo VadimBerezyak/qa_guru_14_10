24924078a9736a5077e3cd2f54c379ba1b2fa60b
VadimBerezyak <89918671+VadimBerezyak@users.noreply.github.com>
2022-08-31 21:22:49 +0300

    refresh github repo

---
+++ src/test/java/qa/guru/allure/SelenideTest.java
@@ -0,0 +1,36 @@
+package qa.guru.allure;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.*;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.By.linkText;
+
+public class SelenideTest {
+    @Test
+    @DisplayName("Создание Issue для авторизованного пользователя")
+    @Link(value = "Testing", url = "https://github.com")
+    @Severity(SeverityLevel.BLOCKER)
+    @Owner("eroshenkoam")
+    @Story("Создание Issue")
+    @Feature("Issue в репозитории")
+public void issueSearchTest(){
+        SelenideLogger.addListener("allure", new AllureSelenide());
+open("https://github.com");
+$(".header-search-input").click();
+$(".header-search-input").setValue("eroshenkoam/allure-example");
+$(".header-search-input").pressEnter();
+$(linkText("eroshenkoam/allure-example")).click();
+$("#issues-tab").click();
+$("#issue_80_link").shouldHave(Condition.text("e.sh"));
+}
+
+
+}
